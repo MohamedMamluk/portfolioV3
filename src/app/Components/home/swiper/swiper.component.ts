4d9efd620b9bee8c1ffb6f74ce854a9e34bab265
MohamedMamlokAli <mohamedmamlukali@gmail.com>
2022-07-03 12:22:10 +0200

    ADD swiper component

---
+++ src/app/Components/home/swiper/swiper.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+import SwiperCore, {
+  Keyboard,
+  Pagination,
+  Navigation,
+  Virtual,
+  Autoplay,
+} from 'swiper';
+
+SwiperCore.use([Keyboard, Pagination, Navigation, Virtual, Autoplay]);
+@Component({
+  selector: 'app-swiper',
+  templateUrl: './swiper.component.html',
+  styleUrls: ['./swiper.component.css'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class SwiperComponent implements OnInit {
+  slides: string[];
+  slides$ = new BehaviorSubject<string[]>(['']);
+
+  constructor() {
+    this.slides = ['angular-logo', 'React_logo', 'next-logo'];
+  }
+
+  ngOnInit(): void {
+    this.slides$.next(
+      Array.from({ length: 600 }).map((el, index) => `Slide ${index + 1}`)
+    );
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Components/header/header.component';
 import { HomeComponent } from './Components/home/home.component';
+import { SwiperModule } from 'swiper/angular';
+import { SwiperComponent } from './Components/home/swiper/swiper.component';
+import { HomeInfoComponent } from './Components/home/home-info/home-info.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    SwiperComponent,
+    HomeInfoComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, SwiperModule],
   providers: [],
   bootstrap: [AppComponent],
 })

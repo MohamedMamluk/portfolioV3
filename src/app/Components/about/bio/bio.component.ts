8c9066f375401077a08b5c90344025cbf62722a5
MohamedMamlokAli <mohamedmamlukali@gmail.com>
2022-07-04 13:22:48 +0200

    CREATE bio component

---
+++ src/app/Components/about/bio/bio.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-bio',
+  templateUrl: './bio.component.html',
+  styleUrls: ['./bio.component.css'],
+})
+export class BioComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}

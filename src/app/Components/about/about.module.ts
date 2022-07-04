e0ad22205a0d068e8b7776594c1096d30529b2cb
MohamedMamlokAli <mohamedmamlukali@gmail.com>
2022-07-04 19:33:50 +0200

    IMPORT angular material matIcons module

--- src/app/Components/about/about.module.ts
+++ src/app/Components/about/about.module.ts
@@ -6,9 +6,17 @@ import { BioComponent } from './bio/bio.component';
 import { AboutRoutingModule } from './about-routing.module';
 import { InterestsComponent } from './interests/interests.component';
 import { EducationComponent } from './education/education.component';
+import { MatIconModule } from '@angular/material/icon';
+
 @NgModule({
-  declarations: [MainComponent, DropdownComponent, BioComponent, InterestsComponent, EducationComponent],
-  imports: [CommonModule, AboutRoutingModule],
+  declarations: [
+    MainComponent,
+    DropdownComponent,
+    BioComponent,
+    InterestsComponent,
+    EducationComponent,
+  ],
+  imports: [CommonModule, AboutRoutingModule, MatIconModule],
   exports: [],
 })
 export class AboutModule {}

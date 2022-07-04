c6f20d3ea40378849f71a2f3483334bc4a54cf84
MohamedMamlokAli <mohamedmamlukali@gmail.com>
2022-07-04 13:25:02 +0200

    ADD tap Info to the about main component

--- src/app/Components/about/main/main.component.ts
+++ src/app/Components/about/main/main.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { TapInfo } from 'src/app/Modules/tap-info';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.css'],
 })
 export class MainComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  taps: TapInfo[];
+  constructor() {
+    this.taps = [
+      {
+        tapName: 'personal-info',
+        tapDropdownItems: ['bio', 'interests', 'education'],
+        as: 'personal',
+      },
+    ];
   }
 
+  ngOnInit(): void {}
 }

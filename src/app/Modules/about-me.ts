1d27b3e989d76010428d9981d5a91948386959e0
MohamedMamlokAli <mohamedmamlukali@gmail.com>
2022-07-05 08:35:13 +0200

    ADD education model

--- src/app/Modules/about-me.ts
+++ src/app/Modules/about-me.ts
@@ -1,3 +1,12 @@
 export interface AboutMe {
   bio: string;
+  education: Education[];
+}
+export interface Education {
+  educationType: string;
+  place: string;
+  duration: {
+    from: string;
+    to: string;
+  };
 }
--- src/app/Services/about-me.service.ts
+++ src/app/Services/about-me.service.ts
@@ -15,9 +15,63 @@ export class AboutMeService {
       currently doing some voluntary work for
       TEDx Youth  in my area, but
       I am open to work`,
+      education: [
+        {
+          educationType: 'Bachelor of Accountancy',
+          place: 'Cairo University',
+          duration: {
+            from: 'Sept 2015',
+            to: 'May 2019',
+          },
+        },
+        {
+          educationType: 'Web Development Challenger course',
+          place: 'Udacity-FWD',
+          duration: {
+            from: 'July 2020',
+            to: 'September 2020',
+          },
+        },
+        {
+          educationType: 'Programming with JavaScript',
+          place: 'Coursera',
+          duration: {
+            from: 'October 2020',
+            to: 'December 2020',
+          },
+        },
+        {
+          educationType: 'React Tutorial and Projects Course',
+          place: 'udemy',
+          duration: {
+            from: 'January 2021',
+            to: 'February 2021',
+          },
+        },
+
+        {
+          educationType: 'Next.js & React - The Complete Guide',
+          place: 'udemy',
+          duration: {
+            from: 'September 2021',
+            to: 'November 2021',
+          },
+        },
+        {
+          educationType: 'Web development using Angular',
+          place: 'ITI Material',
+          duration: {
+            from: 'May 2022',
+            to: 'June 2022',
+          },
+        },
+      ],
     };
   }
   get bio() {
     return this.aboutme.bio.split(',');
+  }
+  get education() {
+    return this.aboutme.education.reverse();
   }
 }

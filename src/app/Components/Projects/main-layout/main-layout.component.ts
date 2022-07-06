414cc537de3719cbd7ccd89a7c33cf484da02c0c
MohamedMamlokAli <mohamedmamlukali@gmail.com>
2022-07-06 16:02:45 +0200

    INJECT projects service to the main layout and call for data

--- src/app/Components/Projects/main-layout/main-layout.component.ts
+++ src/app/Components/Projects/main-layout/main-layout.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { ProjectData } from 'src/app/Modules/projects-data';
+import { ProjectsService } from 'src/app/Services/projects.service';
 
 @Component({
   selector: 'app-main-layout',
   templateUrl: './main-layout.component.html',
-  styleUrls: ['./main-layout.component.css']
+  styleUrls: ['./main-layout.component.css'],
 })
 export class MainLayoutComponent implements OnInit {
-
-  constructor() { }
+  filter: string = 'all';
+  projects: ProjectData[] = [] as ProjectData[];
+  constructor(private projectsService: ProjectsService) {}
 
   ngOnInit(): void {
+    this.projects = this.projectsService.projects(this.filter);
   }
-
 }

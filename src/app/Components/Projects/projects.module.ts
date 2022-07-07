d082fdb5c1dd7b16787f430d0ec6c1fcb2813f39
MohamedMamlokAli <mohamedmamlukali@gmail.com>
2022-07-07 07:36:59 +0200

    REMOVE test component

--- src/app/Components/Projects/projects.module.ts
+++ src/app/Components/Projects/projects.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainLayoutComponent } from './main-layout/main-layout.component';
-import { VueComponent } from './vue/vue.component';
 import { ProjectsRoutingModule } from './projects-routing.module';
 import { ProjectComponent } from './project/project.component';
 
 @NgModule({
-  declarations: [MainLayoutComponent, VueComponent, ProjectComponent],
+  declarations: [MainLayoutComponent, ProjectComponent],
   imports: [CommonModule, ProjectsRoutingModule],
 })
 export class ProjectsModule {}
